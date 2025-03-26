c0d9e62e4d6ddfaad780499c3108ae1d7a72e654
ArikReuter <arik_reuter@gmx.de>
2025-03-26 18:26:12 +0100

    sf

--- LinearRegression/ComparisonModels/PyroMAPPredictor.py
+++ LinearRegression/ComparisonModels/PyroMAPPredictor.py
@@ -2,24 +2,54 @@ from pyro.infer.autoguide import  AutoDelta
 from PFNExperiments.LinearRegression.ComparisonModels.Variational_InferenceAutoguide import Variational_InferenceAutoguide
 import torch
 
+from pyro.infer.autoguide import  AutoDelta
+from PFNExperiments.LinearRegression.ComparisonModels.Variational_InferenceAutoguide import Variational_InferenceAutoguide
+import torch
+
 class PyroMAPPredictor():
     """
     Class that takes a pprogram and then becomes a MAP predictor.
     """
 
+    ppgrogram_name2response_function = {
+    "ig": lambda x: x,
+    "ig_intercept": lambda x: x,
+    "Laplace_ig": lambda x: x,
+    "Laplace_ig_intercept": lambda x: x,
+    "Gamma_ig": lambda x: x,
+    "logreg_ig": lambda x: torch.sigmoid(x),
+    "ig_gamma_response_reparam": lambda x: torch.exp(x),
+    }
+
+    ppgrogram_name2useintercept = {
+        "ig": False,
+        "ig_intercept": True,
+        "Laplace_ig": False,
+        "Laplace_ig_intercept": True,
+        "Gamma_ig": False,
+        "logreg_ig": False,
+        "ig_gamma_response_reparam": False,
+    }
+
     def __init__(
             self,
             pprogram_y,
-            lr:float = 1e-2,
+            pprogram_name: str,
+            lr:float = 1e-1,
             n_steps:int = 2000
             ):
         """
         Args:
             pprogram_y: a probabilistic program for the response variable
+            pprogram_name: str: the name of the probabilistic program
             lr: float: the learning rate
             n_steps: int: the number of steps
         """
         self.pprogram_y = pprogram_y
+
+        self.response_function = self.ppgrogram_name2response_function[pprogram_name]
+        self.use_intercept = self.ppgrogram_name2useintercept[pprogram_name]
+
 
         self.vi_MAP = Variational_InferenceAutoguide(
         pprogram=pprogram_y,
@@ -44,10 +74,16 @@ class PyroMAPPredictor():
         """
         predict the MAP estimate
         """
-        beta = self.map_estimates["beta"]
-        beta0 = self.map_estimates["beta0"]
+        beta = self.map_estimates["beta"].squeeze()
+        
+        if self.use_intercept:
+            intercept = self.map_estimates["beta0"].squeeze()
 
-        return torch.matmul(X,beta) + beta0
+            return self.response_function(torch.matmul(X,beta) + intercept)
+        else:
+            return self.response_function(torch.matmul(X,beta))
+        
+        
 
 
         
