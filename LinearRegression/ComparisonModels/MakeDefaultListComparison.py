5f6b8406dfef67ffde21414c287faba5ad90f167
ArikReuter <arik_reuter@gmx.de>
2025-03-27 19:17:32 +0100

    undo changes

--- LinearRegression/ComparisonModels/MakeDefaultListComparison.py
+++ LinearRegression/ComparisonModels/MakeDefaultListComparison.py
@@ -1,5 +1,5 @@
 from PFNExperiments.LinearRegression.ComparisonModels.Hamiltionion_MC import Hamiltionian_MC
-from pyro.infer.autoguide import AutoDiagonalNormal, AutoMultivariateNormal, AutoLaplaceApproximation, AutoIAFNormal, AutoStructured, AutoDelta
+from pyro.infer.autoguide import AutoDiagonalNormal, AutoMultivariateNormal, AutoLaplaceApproximation, AutoIAFNormal, AutoStructured
 from PFNExperiments.LinearRegression.ComparisonModels.Variational_InferenceAutoguide import Variational_InferenceAutoguide
 
 
@@ -19,40 +19,40 @@ def make_default_list_comparison(
     vi_diag = Variational_InferenceAutoguide(
         pprogram=pprogram_y,
         make_guide_fun=AutoDiagonalNormal,
-        n_steps=5000,
+        n_steps=2000,
         n_samples=n_samples,
-        lr=1
+        lr=1e-2
     )
 
     vi_multivariate_normal = Variational_InferenceAutoguide(
         pprogram=pprogram_y,
         make_guide_fun=AutoMultivariateNormal,
-        n_steps=5000,
+        n_steps=2000,
         n_samples=n_samples,
-        lr=1
+        lr=1e-2
     )
     vi_laplace = Variational_InferenceAutoguide(
         pprogram=pprogram_y,
         make_guide_fun=AutoLaplaceApproximation,
-        n_steps=5000,
+        n_steps=2000,
         n_samples=n_samples,
-        lr=1
+        lr=1e-2
     )
 
     vi_autoIAF = Variational_InferenceAutoguide(
         pprogram=pprogram_y,
         make_guide_fun=AutoIAFNormal,
-        n_steps=5000,
+        n_steps=2000,
         n_samples=n_samples,
-        lr=1e-1
+        lr=1e-3
     )
 
     vi_autostrucured = Variational_InferenceAutoguide(
         pprogram=pprogram_y,
         make_guide_fun=AutoStructured,
-        n_steps=5000,
+        n_steps=2000,
         n_samples=n_samples,
-        lr=1
+        lr=1e-2
     )
 
     model_list = [
@@ -81,17 +81,17 @@ def make_reduced_list_comparison(
     vi_diag = Variational_InferenceAutoguide(
         pprogram=pprogram_y,
         make_guide_fun=AutoDiagonalNormal,
-        n_steps=5000,
+        n_steps=2000,
         n_samples=n_samples,
-        lr=1
+        lr=1e-2
     )
 
     vi_multivariate_normal = Variational_InferenceAutoguide(
         pprogram=pprogram_y,
         make_guide_fun=AutoMultivariateNormal,
-        n_steps=5000,
+        n_steps=2000,
         n_samples=n_samples,
-        lr=1
+        lr=1e-2
     )
 
     model_list = [
@@ -100,93 +100,3 @@ def make_reduced_list_comparison(
     ]
 
     return model_list
-
-
-def make_vi_list_lr(
-        pprogram_y,
-        n_samples: int = 1000,
-        n_steps: int = 2000,
-        lr: float = 1e-3,
-        print_lr: bool = False
-        ):
-    """
-    Create the default list of comparison models.
-    Args:
-        pprogram_y: a probabilistic program for the response variable
-        n_steps : int: the number of steps to take in the optimization
-        n_samples: int: the number of samples to draw
-        lr: float: the learning rate of the optimizer
-        print_lr: bool: whether to print the learning rate
-    """
-
-    vi_diag = Variational_InferenceAutoguide(
-        pprogram=pprogram_y,
-        make_guide_fun=AutoDiagonalNormal,
-        n_steps=n_steps,
-        n_samples=n_samples,
-        lr=lr,
-        print_lr=print_lr   
-    )
-
-    vi_multivariate_normal = Variational_InferenceAutoguide(
-        pprogram=pprogram_y,
-        make_guide_fun=AutoMultivariateNormal,
-        n_steps=n_steps,
-        n_samples=n_samples,
-        lr=lr,
-        print_lr=print_lr
-    )
-    vi_laplace = Variational_InferenceAutoguide(
-        pprogram=pprogram_y,
-        make_guide_fun=AutoLaplaceApproximation,
-        n_steps=n_steps,
-        n_samples=n_samples,
-        lr=lr,
-        print_lr=print_lr
-    )
-
-    vi_autoIAF = Variational_InferenceAutoguide(
-        pprogram=pprogram_y,
-        make_guide_fun=AutoIAFNormal,
-        n_steps=n_steps,
-        n_samples=n_samples,
-        lr=lr,
-        print_lr=print_lr
-    )
-
-    vi_autostrucured = Variational_InferenceAutoguide(
-        pprogram=pprogram_y,
-        make_guide_fun=AutoStructured,
-        n_steps=n_steps,
-        n_samples=n_samples,
-        lr=lr,
-        print_lr=print_lr
-    )
-
-    model_list = [
-        vi_diag,
-        vi_multivariate_normal,
-        vi_laplace,
-        vi_autoIAF,
-        vi_autostrucured
-    ]
-
-    return model_list
-
-
-def make_MAP_model(
-        pprogram_y
-        ):
-    """
-    Create the MAP model.
-    Args:
-        pprogram_y: a probabilistic program for the response variable
-    """
-    vi_MAP = Variational_InferenceAutoguide(
-        pprogram=pprogram_y,
-        make_guide_fun=AutoDelta,
-        n_steps=5000,
-        n_samples=1,
-        lr=1e-1
-    )
-    return vi_MAP
--- LinearRegression/ComparisonModels/Variational_InferenceAutoguide.py
+++ LinearRegression/ComparisonModels/Variational_InferenceAutoguide.py
@@ -1,4 +1,4 @@
-import pyro
+import pyro 
 import torch
 
 import pyro.infer
@@ -10,32 +10,24 @@ from PFNExperiments.LinearRegression.ComparisonModels.PosteriorComparisonModel i
 
 from pyro.infer.autoguide import AutoDiagonalNormal
 
-from pyro.optim import PyroOptim
-import torch.optim
-
 class Variational_InferenceAutoguide(PosteriorComparisonModel):
     """
     Perform variational inference on a given probabilistic program
     """
 
-    def __init__(self,
+    def __init__(self, 
                  pprogram: ppgram_linear_model_return_y,
                  make_guide_fun: callable = AutoDiagonalNormal,
                  additional_make_guide_args: dict = {},
-                 optim:str = "Adam",
-                 n_steps:int = 1000,
+                 n_steps:int = 2000,
                  n_samples:int = 200,
-                 lr: float = 1e-1,
-                 print_lr: bool = False) -> None:
+                 lr: float = 1e-3) -> None:
         """
         Args:
             pprogram: ppgram_linear_model_return_y: the probabilistic program
             make_guide_fun : callable: a function that generates the guide
-            additional_make_guide_args: dict: additional arguments to the make_guide_fun
-            optim: str: the optimizer to use can be "ClippedAdam" or "L-BFGS" 
             n_steps: int: the number of steps to take in the optimization
             lr: float: the learning rate of the optimizer
-            print_lr: bool: whether to print the learning rate
 
         Returns:
             None
@@ -46,9 +38,8 @@ class Variational_InferenceAutoguide(PosteriorComparisonModel):
         self.n_steps = n_steps
         self.n_samples = n_samples
         self.lr = lr
-        self.print_lr = print_lr
-        self.optim_str = optim
     
+
     def generate_guide(self):
         """
         generate the guide
@@ -57,7 +48,7 @@ class Variational_InferenceAutoguide(PosteriorComparisonModel):
         self.guide = self.make_guide_fun(self.pprogram, **self.additional_make_guide_args)
 
 
-    def do_inference(self,
+    def do_inference(self,  
                 X: torch.Tensor,
                 y: torch.Tensor) -> torch.Tensor:
         """
@@ -68,6 +59,13 @@ class Variational_InferenceAutoguide(PosteriorComparisonModel):
         Returns:
             torch.Tensor: the samples from the posterior distribution
         """
+        self.guide = self.make_guide_fun(self.pprogram)
+
+
+        self.optimizer = pyro.optim.Adam({"lr": self.lr})
+
+        self.svi = SVI(self.pprogram, self.guide, self.optimizer, loss=Trace_ELBO())
+
         pyro.clear_param_store()
         for step in range(self.n_steps):
             self.loss = self.svi.step(X, y)
@@ -75,7 +73,7 @@ class Variational_InferenceAutoguide(PosteriorComparisonModel):
                 print('.', end='')
         print()
         return self.loss
-
+    
     def sample_posterior(self,
                 X: torch.Tensor,
                 y: torch.Tensor) -> torch.Tensor:
@@ -87,44 +85,10 @@ class Variational_InferenceAutoguide(PosteriorComparisonModel):
         Returns:
             torch.Tensor: the samples from the posterior distribution
         """
-
-        self.guide = self.make_guide_fun(self.pprogram)
-
-        lr = self.lr
-        n_steps = self.n_steps
-
-
-        if self.optim_str == "L-BFGS":
-            self.optimizer = PyroOptim(torch.optim.LBFGS, {"lr": lr, "max_iter": n_steps})
-
-        elif self.optim_str == "Adam":
-            initial_lr = lr
-            gamma = 1e-3  # final learning rate will be gamma * initial_lr
-            lrd = gamma ** (1 / n_steps)
-            self.optimizer = pyro.optim.ClippedAdam({'lr': initial_lr, 'lrd': lrd})
-        else: 
-            print(f"optimizer {self.optim_str} not recognized, using Adam")
-            initial_lr = lr
-            gamma = 1e-3  # final learning rate will be gamma * initial_lr
-            lrd = gamma ** (1 / n_steps)
-            self.optimizer = pyro.optim.ClippedAdam({'lr': initial_lr, 'lrd': lrd})
-            
-
-
-        self.svi = SVI(self.pprogram, self.guide, self.optimizer, loss=Trace_ELBO())
-
         self.do_inference(X, y)
         posterior_samples = pyro.infer.Predictive(self.guide, num_samples=self.n_samples)(X, y)
         return posterior_samples
-
+    
     def __repr__(self) -> str:
-
-        self.guide = self.make_guide_fun(self.pprogram, **self.additional_make_guide_args)
-
-
-        rep = "Variational Inference with guide: {}".format(self.guide)
-
-        if self.print_lr:
-            rep = f"Variational Inference with learning rate: {self.lr} and guide: {self.guide}"
-
-        return rep
+        self.guide = self.make_guide_fun(self.pprogram)
+        return "Variational Inference with guide: {}".format(self.guide)
