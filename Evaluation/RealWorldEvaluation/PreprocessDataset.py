74e699a0870bd7b8959897abc2ab624987c4043e
ArikReuter <arik_reuter@gmx.de>
2025-04-06 09:13:54 +0200

    sf

--- Evaluation/RealWorldEvaluation/PreprocessDataset.py
+++ Evaluation/RealWorldEvaluation/PreprocessDataset.py
@@ -458,13 +458,13 @@ class PreprocessorGammaResponse():
         y = y - y.min() + 1e-5
 
         # bring vriance 
-        y = y / (y.std() + 1e-5)	
+        #y = y / (y.std() + 1e-5)	
 
         y_test = torch.exp(y_test)
         y_test = boxcox(y_test, self.target_lambda)
         y_test = torch.tensor(y_test, dtype = torch.float)
         y_test = y_test - y_test.min() + 1e-5
-        y_test = y_test / (y_test.std() + 1e-5)
+        #y_test = y_test / (y_test.std() + 1e-5)
 
         new_dataset = {
             "x": x,
