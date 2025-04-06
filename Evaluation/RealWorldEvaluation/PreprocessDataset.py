848d2267addf9b4f693b6eebd3c10254680c9dba
ArikReuter <arik_reuter@gmx.de>
2025-04-06 16:32:32 +0200

    sf

--- Evaluation/RealWorldEvaluation/PreprocessDataset.py
+++ Evaluation/RealWorldEvaluation/PreprocessDataset.py
@@ -449,8 +449,10 @@ class PreprocessorGammaResponse():
         y = self.target_scaler(y)
         y_test = self.target_scaler(y_test)
 
-        #y = torch.exp(y) # the target is the log of the response
-        #y = boxcox(y, self.target_lambda)
+        y = y / (y.std() + 1e-5)
+
+        y = torch.exp(y) # the target is the log of the response
+        y = boxcox(y, self.target_lambda)
 
         y = torch.tensor(y, dtype = torch.float)
 
@@ -460,11 +462,14 @@ class PreprocessorGammaResponse():
         # bring vriance 
         y = y / (y.std() + 1e-5)	
 
+        
+        y_test = y_test / (y_test.std() + 1e-5)
         y_test = torch.exp(y_test)
-        #y_test = boxcox(y_test, self.target_lambda)
+        y_test = boxcox(y_test, self.target_lambda)
         y_test = torch.tensor(y_test, dtype = torch.float)
         y_test = y_test - y_test.min() + 1e-5
         y_test = y_test / (y_test.std() + 1e-5)
+        
 
         new_dataset = {
             "x": x,
